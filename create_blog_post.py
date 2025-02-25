919c0361b60a7d5b134406633966851f942b6783
Danny Guan <dannyguan21@gmail.com>
2025-02-25 15:15:52 -0500

    Update create_blog_post.py

--- create_blog_post.py
+++ create_blog_post.py
@@ -20,43 +20,54 @@ def create_blog_post_from_args():
     blog_amd_applications = args[11]
     blog_amd_industries = args[12]
 
-    metadata = {
-        "blogpost": False,
-        "blog_title": blog_title,
-        "date": blog_release_date,
-        "author": blog_authors,
-        "thumbnail": "",
-        "tags": blog_tags,
-        "category": blog_category,
-        "target_audience": blog_audience,
-        "key_value_propositions": blog_kvp,
-        "language": "English",
-        "myst": {
-            "html_meta": {
-                "author": blog_authors,
-                "description lang=en": blog_title,
-                "keywords": blog_keywords,
-                "property=og:locale": "en_US",
-                "amd_category": "",
-                "amd_asset_type": "",
-                "amd_blog_type": "",
-                "amd_technical_blog_type": blog_amd_technical_blog_type,
-                "amd_developer_type": blog_amd_developer_type,
-                "amd_deployment": "",
-                "amd_product_type": blog_amd_product_type,
-                "amd_developer_tool": "",
-                "amd_applications": blog_amd_applications,
-                "amd_industries": blog_amd_industries,
-                "amd_blog_releasedate": blog_release_date,
-            }
-        }
-    }
+    blog_template = """
+---
+blogpost: false
+blog_title: "{blog_title}"
+date: {blog_release_date}
+author: '{blog_author}'
+thumbnail: ''
+tags: {blog_tags}
+category: {blog_category}
+target_audience: {blog_audience}
+key_value_propositions: {blog_key_value_proposition}
+language: English
+myst:
+    html_meta:
+        "author": "Danny Guan"
+        "description lang=en": "Guide to ROCm Blogs Metadata"
+        "keywords": "'{blog_keywords}'"
+        "property=og:locale": "en_US"
+        "amd_category": "Developer Resources"
+        "amd_asset_type": "Blogs"
+        "amd_blog_type": "Technical Articles & Blogs"
+        "amd_technical_blog_type": "'{blog_amd_technical_blog_type}'"
+        "amd_developer_type": "'{blog_amd_developer_type}'"
+        "amd_deployment": "Servers"
+        "amd_product_type": "'{blog_amd_product_type}'"
+        "amd_developer_tool": "ROCm Software, Open-Source Tools"
+        "amd_applications": "'{blog_amd_applications}'"
+        "amd_industries": "'{blog_amd_industries}'"
+        "amd_blog_releasedate": Tue April 16, 12:00:00 PST 2024
+---
+"""
+    blog_template = blog_template.format(
+        blog_title=blog_title,
+        blog_release_date=blog_release_date,
+        blog_author=blog_author,
+        blog_tags=blog_tags,
+        blog_category=blog_category,
+        blog_audience=blog_audience,
+        blog_key_value_proposition=blog_key_value_proposition,
+        blog_keywords=blog_keywords,
+        blog_amd_technical_blog_type=blog_amd_technical_blog_type,
+        blog_amd_product_type=blog_amd_product_type,
+        blog_amd_developer_type=blog_amd_developer_type,
+        blog_amd_applications=blog_amd_applications,
+        blog_amd_industries=blog_amd_industries,
+    )
 
-    # Now proceed with creating the new blog post using metadata.
-    print("Metadata received:")
-    for key, value in metadata.items():
-        print(f"{key}: {value}")
-    # ... (rest of your blog post creation code) ...
+    print(blog_template)
 
 if __name__ == "__main__":
     create_blog_post_from_args()
