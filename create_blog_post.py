d3ad00c7f885a7df2c7cea2b29b20d9a72799162
Danny Guan <dannyguan21@gmail.com>
2025-02-25 15:15:02 -0500

    Update create_blog_post.py

--- create_blog_post.py
+++ create_blog_post.py
@@ -1,73 +1,62 @@
 import sys
 
+def create_blog_post_from_args():
+    # sys.argv[0] is the script name; the rest are your inputs
+    args = sys.argv[1:]
+    if len(args) < 13:
+        print("Not enough arguments provided.")
+        sys.exit(1)
+    blog_title = args[0]
+    blog_release_date = args[1]
+    blog_authors = args[2]
+    blog_tags = args[3]
+    blog_category = args[4]
+    blog_audience = args[5]
+    blog_kvp = args[6]
+    blog_keywords = args[7]
+    blog_amd_technical_blog_type = args[8]
+    blog_amd_product_type = args[9]
+    blog_amd_developer_type = args[10]
+    blog_amd_applications = args[11]
+    blog_amd_industries = args[12]
 
-def create_blog(blog_title, blog_release_date, blog_author, blog_tags, blog_category, blog_audience, blog_key_value_proposition, blog_keywords, blog_amd_technical_blog_type, blog_amd_product_type, blog_amd_developer_type, blog_amd_applications, blog_amd_industries):
-    # grab metadata from bash input
-    blog_template = """
----
-blogpost: false
-blog_title: "{blog_title}"
-date: {blog_release_date}
-author: '{blog_author}'
-thumbnail: ''
-tags: {blog_tags}
-category: {blog_category}
-target_audience: {blog_audience}
-key_value_propositions: {blog_key_value_proposition}
-language: English
-myst:
-    html_meta:
-        "author": "Danny Guan"
-        "description lang=en": "Guide to ROCm Blogs Metadata"
-        "keywords": "'{blog_keywords}'"
-        "property=og:locale": "en_US"
-        "amd_category": "Developer Resources"
-        "amd_asset_type": "Blogs"
-        "amd_blog_type": "Technical Articles & Blogs"
-        "amd_technical_blog_type": "'{blog_amd_technical_blog_type}'"
-        "amd_developer_type": "'{blog_amd_developer_type}'"
-        "amd_deployment": "Servers"
-        "amd_product_type": "'{blog_amd_product_type}'"
-        "amd_developer_tool": "ROCm Software, Open-Source Tools"
-        "amd_applications": "'{blog_amd_applications}'"
-        "amd_industries": "'{blog_amd_industries}'"
-        "amd_blog_releasedate": Tue April 16, 12:00:00 PST 2024
----
-"""
-    blog_template = blog_template.format(
-        blog_title=blog_title,
-        blog_release_date=blog_release_date,
-        blog_author=blog_author,
-        blog_tags=blog_tags,
-        blog_category=blog_category,
-        blog_audience=blog_audience,
-        blog_key_value_proposition=blog_key_value_proposition,
-        blog_keywords=blog_keywords,
-        blog_amd_technical_blog_type=blog_amd_technical_blog_type,
-        blog_amd_product_type=blog_amd_product_type,
-        blog_amd_developer_type=blog_amd_developer_type,
-        blog_amd_applications=blog_amd_applications,
-        blog_amd_industries=blog_amd_industries,
-    )
+    metadata = {
+        "blogpost": False,
+        "blog_title": blog_title,
+        "date": blog_release_date,
+        "author": blog_authors,
+        "thumbnail": "",
+        "tags": blog_tags,
+        "category": blog_category,
+        "target_audience": blog_audience,
+        "key_value_propositions": blog_kvp,
+        "language": "English",
+        "myst": {
+            "html_meta": {
+                "author": blog_authors,
+                "description lang=en": blog_title,
+                "keywords": blog_keywords,
+                "property=og:locale": "en_US",
+                "amd_category": "",
+                "amd_asset_type": "",
+                "amd_blog_type": "",
+                "amd_technical_blog_type": blog_amd_technical_blog_type,
+                "amd_developer_type": blog_amd_developer_type,
+                "amd_deployment": "",
+                "amd_product_type": blog_amd_product_type,
+                "amd_developer_tool": "",
+                "amd_applications": blog_amd_applications,
+                "amd_industries": blog_amd_industries,
+                "amd_blog_releasedate": blog_release_date,
+            }
+        }
+    }
 
-    print(blog_template)
-
+    # Now proceed with creating the new blog post using metadata.
+    print("Metadata received:")
+    for key, value in metadata.items():
+        print(f"{key}: {value}")
+    # ... (rest of your blog post creation code) ...
 
 if __name__ == "__main__":
-    # Read entire input from stdin
-    blog_title = input()
-    blog_release_date = input()
-    blog_author = input()
-    blog_tags = input()
-    blog_category = input()
-    blog_audience = input()
-    blog_key_value_proposition = input()
-    blog_keywords = input()
-    blog_amd_technical_blog_type = input()
-    blog_amd_product_type = input()
-    blog_amd_developer_type = input()
-    blog_amd_applications = input()
-    blog_amd_industries = input()
-
-    # Extract the metadata from the list
-    create_blog(blog_title, blog_release_date, blog_author, blog_tags, blog_category, blog_audience, blog_key_value_proposition, blog_keywords, blog_amd_technical_blog_type, blog_amd_product_type, blog_amd_developer_type, blog_amd_applications, blog_amd_industries)
+    create_blog_post_from_args()
